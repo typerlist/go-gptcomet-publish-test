ea2f69a830e3ac17bbbc168bbf5c66167a4c0b9d
belingud <1170202353@qq.com>
2025-01-01 22:46:22 +0800

    chore: delete useless codes

--- pkg/config/types.go
+++ pkg/config/types.go
@@ -5,14 +5,3 @@ type ConfigRequirement struct {
 	DefaultValue  string
 	PromptMessage string
 }
-
-// ProviderConfig represents provider-specific configuration
-type ProviderConfig struct {
-	APIBase    string
-	Model      string
-	MaxTokens  int
-	Location   string
-	ProjectID  string
-	APIKey     string
-	APIVersion string
-}
