04c02fd9cc30b806f2728ba7c38e1d96938d45cf
belingud <1170202353@qq.com>
2025-01-03 23:24:29 +0800

    ci: Update release workflow and binary naming

--- py/gptcomet/__init__.py
+++ py/gptcomet/__init__.py
@@ -2,13 +2,31 @@ import os
 import shlex
 import subprocess
 import sys
-
+import platform
 
 def find_gptcomet_binary():
-    binary_name = "gptcomet.exe" if sys.platform == "win32" else "gptcomet"
+    platform_name = sys.platform
+    
+    machine = platform.machine().lower()
+    if machine in ('arm64', 'aarch64', 'arm'):
+        arch = 'arm'
+    elif machine in ('x86_64', 'amd64', 'x64', 'i386', 'x86'):
+        arch = 'x86_64'
+    else:
+        raise OSError(f"Unsupported architecture: {machine}")
+    
+    if platform_name == "win32":
+        binary_name = f"gptcomet_{arch}.exe"
+    elif platform_name == "darwin":
+        binary_name = f"gptcomet_{arch}_mac"
+    elif platform_name == "linux":
+        binary_name = f"gptcomet_{arch}_linux"
+    else:
+        raise OSError(f"Unsupported platform: {platform_name}")
+    
     binary_path = os.path.join(os.path.dirname(__file__), "bin", binary_name)
     if not os.path.isfile(binary_path):
-        msg = "gptcomet binary not found, this version of gptcomet may be incomplete, please open an issue on github, thanks."
+        msg = f"gptcomet binary not found for {platform_name}-{arch}, please open an issue on github, thanks."
         raise FileNotFoundError(msg)
     return binary_path
 
